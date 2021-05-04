a2fb114c9edb495362e5875fc3ca486b2a4b26aa
filipp <zeliboba0@bk.ru>
2021-05-04 23:10:29 +0300

    shlyapa

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -2,18 +2,18 @@ package com.company;
 
 public class Test {
     public static void main(String[] args) {
-    Cat cat=new Cat("Кот");
+        Cat cat = new Cat("Кот");
         System.out.println(cat.name);
-    cat.Swim(0);
-    cat.Run(200);
+        cat.Swim(0);
+        cat.Run(200);
 
         System.out.println();
-    Dog dog=new Dog("Пёс");
+        Dog dog = new Dog("Пёс");
         System.out.println(dog.name);
-    dog.Swim(10);
-    dog.Run(500);
+        dog.Swim(10);
+        dog.Run(500);
+        System.out.println();
+        System.out.println("шляпа какая-то с комиченьем, пушиньем и пулреквестиньем");
 
-
-
-}
+    }
 }
