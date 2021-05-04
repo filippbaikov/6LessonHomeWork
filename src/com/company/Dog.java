4c76b8bdd266694a7401bc4f0d7656d727b093da
filipp <zeliboba0@bk.ru>
2021-05-04 19:49:29 +0300

    Initial commit

---
+++ src/com/company/Dog.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Dog extends Animal {
+
+
+    public Dog(String name) {
+        super(name);
+
+
+    }
+
+}
+
---
+++ src/com/company/Test.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+    Cat cat=new Cat("Кот");
+        System.out.println(cat.name);
+    cat.Swim(0);
+    cat.Run(200);
+   // cat.r
+        System.out.println();
+    Dog dog=new Dog("Пёс");
+        System.out.println(dog.name);
+    dog.Swim(10);
+    dog.Run(500);
+
+
+
+}
+}
